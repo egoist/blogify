b278d2e45e556c93a4c80e5066bdcc8700f0970b
EGOIST <0x142857@gmail.com>
2020-12-08 15:48:53 +0800

    add umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -13,6 +15,14 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {isProd && (
+            <script
+              async
+              defer
+              data-website-id="9939e642-1de9-4fdc-9407-c1cbd39cda4a"
+              src="https://umami-stats.vercel.app/umami.js"
+            ></script>
+          )}
         </body>
       </Html>
     )
